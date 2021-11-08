6b50619b58383da186ee02423c8516be59eeb3ff
harry <harry.gambot@gmail.com>
2021-11-08 19:40:43 +0700

    add footer on layout component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,19 +1,19 @@
-import { Fragment } from "react"
-import Copyright from "../copyright/Copyright"
-import MainHeader from "./main-header"
+import { Fragment } from "react";
+import Copyright from "../copyright/Copyright";
+import MainHeader from "./main-header";
 
-function Layout(props){
-    return(
-        <Fragment>
-            <header>
-            <MainHeader/>
-            </header>
-            <main>
-                {props.children}
-            </main>
-            <Copyright/>
-        </Fragment>
-    )
+function Layout(props) {
+  return (
+    <Fragment>
+      <header>
+        <MainHeader />
+      </header>
+      <main>{props.children}</main>
+      <footer>
+        <Copyright />
+      </footer>
+    </Fragment>
+  );
 }
 
-export default Layout
+export default Layout;
